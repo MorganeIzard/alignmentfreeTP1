3fbd0070614eff03db185c5688da057ff1615660
MorganeIzard <117927164+MorganeIzard@users.noreply.github.com>
2022-11-17 21:32:52 +0100

    Update kmers.py

--- kmers.py
+++ kmers.py
@@ -16,5 +16,31 @@ def kmer2str(val, k):
 
 
 def stream_kmers(text, k):
-    # --- To complete ---
-    pass
+    list_kmer = []
+    kmer = 0
+    rev_kmer = 0
+
+    for i in range(k):
+        kmer = kmer << 2
+        kmer += encore(text[i])
+
+        rev_kmer += (3 - encode(text[i])) * 2**(2*i)
+
+    list_kmer.append(min(kmer, rev_kmer))
+
+    mask = (1<<(k-1)*2)-1
+    for nucl in text[k:]:
+        kmer = kmer & mask
+        kmer = kmer << 2
+        kmer = kmer + encode(text[nucl])
+
+        rev_kmer = reverse_kmer >> 2
+        rev_kmer += (3 - encode(nucl)) * (2**(2*k))
+        list_kmer.append(min(kmer, rev_kmer))
+
+    return list_kmer
+
+def encode(x) :
+    dico = {'A':0, 'C':1, 'T':2, 'G':3}
+    if x not in dico.keys() :
+        return 0
