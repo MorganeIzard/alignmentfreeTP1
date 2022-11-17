579962edb0bdfc15fd7e9b01760516a17c985384
MorganeIzard <117927164+MorganeIzard@users.noreply.github.com>
2022-11-18 00:27:25 +0100

    Update kmers.py

--- kmers.py
+++ kmers.py
@@ -1,19 +1,9 @@
-
-def kmer2str(val, k):
-    """ Transform a kmer integer into a its string representation
-    :param int val: An integer representation of a kmer
-    :param int k: The number of nucleotides involved into the kmer.
-    :return str: The kmer string formatted
-    """
-    letters = ['A', 'C', 'T', 'G']
-    str_val = []
-    for i in range(k):
-        str_val.append(letters[val & 0b11])
-        val >>= 2
-
-    str_val.reverse()
-    return "".join(str_val)
-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@teacher: yoann dufresne
+@students: camillo and izard
+"""
 
 def stream_kmers(text : str, k : int) -> list:
     '''
@@ -40,7 +30,7 @@ def stream_kmers(text : str, k : int) -> list:
     for nucl in text[k:]:
         kmer = kmer & mask
         kmer = kmer << 2
-        kmer = kmer + encode(text[nucl])
+        kmer = kmer + encode(nucl)
 
         rev_kmer = rev_kmer >> 2
         rev_kmer += (3 - encode(nucl)) * (2**(2*k))
