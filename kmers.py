fe7eef0e7b95634be89cc9b42813246e5eef04da
MorganeIzard <117927164+MorganeIzard@users.noreply.github.com>
2022-11-28 01:45:35 +0100

    Update kmers.py

--- kmers.py
+++ kmers.py
@@ -4,59 +4,20 @@
 @teacher: yoann dufresne
 @students: camillo and izard
 """
+from alive_progress import alive_bar
 
-def stream_kmers(text : str, k : int) -> list:
-    '''
-    Stream kmers from a text
-    ------------
-    parameters
-    text : a string
-    k : the length of the kmers
-    ------------
-    output
-    a list of kmers
-    '''
-    list_kmer, kmer, rev_kmer = [], 0, 0
-
+def kmer2str(val, k):
+    letters = ['A', 'C', 'T', 'G']
+    str_val = []
     for i in range(k):
-        kmer = kmer << 2
-        kmer += encode(text[i])
+        str_val.append(letters[val & 0b11])
+        val >>= 2
 
-        rev_kmer += (3 - encode(text[i])) * 2**(2*i)
+    str_val.reverse()
+    return "".join(str_val)
 
-    list_kmer.append(min(kmer, rev_kmer))
 
-    mask = (1<<(k-1)*2)-1
-    for nucl in text[k:]:
-        kmer = kmer & mask
-        kmer = kmer << 2
-        kmer = kmer + encode(nucl)
-
-        rev_kmer = rev_kmer >> 2
-        rev_kmer += (3 - encode(nucl)) * (2**(2*k))
-        list_kmer.append(min(kmer, rev_kmer))
-
-    return list_kmer
-
-def encode(x : str) -> int:
-    '''
-    Encode a nucleotide into a number
-    ------------
-    parameter
-    x : a nucleotide
-    ------------
-    output
-    the number corresponding to the nucleotide
-    '''
-    dico = {'A':0, 'C':1, 'T':2, 'G':3}
-    if x not in dico.keys() :
-        return 0
-    else :
-        return dico[x]
-    
-    ############CORRECTION##############
-    
-    def stream_kmers(text, k):
+def stream_kmers(text, k):
     # Precompute the (k-1)-mer (and reverse)
     kmer = 0
     rkmer = 0
@@ -86,12 +47,53 @@ def encode(x : str) -> int:
 
         yield kmer, rkmer
 
-def kmer2str(val, k):
-    letters = ['A', 'C', 'T', 'G']
-    str_val = []
-    for i in range(k):
-        str_val.append(letters[val & 0b11])
-        val >>= 2
+def stream_kmers_ini(text : str, k : int, bar) -> list:
+    '''
+    Stream kmers from a text
+    ------------
+    parameters
+    text : a string
+    k : the length of the kmers
+    ------------
+    output
+    a list of kmers
+    '''
+    list_kmer, kmer, rev_kmer = [], 0, 0
 
-    str_val.reverse()
-    return "".join(str_val)
+    for i in range(k):
+        kmer = kmer << 2
+        kmer += encode(text[i])
+
+        rev_kmer += (3 - encode(text[i])) * 2**(2*i)
+    bar()
+    list_kmer.append(min(kmer, rev_kmer))
+
+    mask = (1<<(k-1)*2)-1
+    for nucl in text[k:]:
+        kmer = kmer & mask
+        kmer = kmer << 2
+        kmer = kmer + encode(nucl)
+
+        rev_kmer = rev_kmer >> 2
+        rev_kmer += (3 - encode(nucl)) * (2**(2*k))
+        list_kmer.append(min(kmer, rev_kmer))
+
+        bar()
+
+    return list_kmer
+
+def encode(x : str) -> int:
+    '''
+    Encode a nucleotide into a number
+    ------------
+    parameter
+    x : a nucleotide
+    ------------
+    output
+    the number corresponding to the nucleotide
+    '''
+    dico = {'A':0, 'C':1, 'T':2, 'G':3}
+    if x not in dico.keys() :
+        return 0
+    else :
+        return dico[x]
